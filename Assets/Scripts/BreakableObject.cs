9b93d22ec07988ac748c022ff5264fc473a9d809
Renee Camillo Pollet <renee.pollet@highstreet.market>
2024-06-27 16:16:01 -0600

    Color and points change over time

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -10,21 +10,55 @@ public class BreakableObject : MonoBehaviour, ITargetable
     private Transform explodePoint;
 
     [SerializeField]
-    private int scoreValue = 1;
+    private int baseScoreValue = 100;
+    private int scoreValue;
 
     public GameEvent Event;
+    public Color startColor = Color.green;
+    public Color endColor = Color.red;
+    private Renderer objRenderer;
+    private Material objMaterial;
 
     [SerializeField]
     private float force = 100f;
     [SerializeField]
     private GameObject[] shardPeices;
 
+    private float timerToShoot = 0f;
+    [SerializeField]
+    private float maxTimeToShoot = 1f;
+
+
     private void Awake()
     {
+        timerToShoot = 0f;
+        objRenderer = FullObj.GetComponentInChildren<Renderer>();
+        if (objRenderer != null)
+        {
+            objMaterial = objRenderer.material;
+            objMaterial.EnableKeyword("_EMISSION"); // Enable emission
+        }
         Switch(true);
-        Destroy(gameObject,7f);
-
+        Destroy(gameObject, 7f);
+        
     }
+
+    void Update()
+    {
+        timerToShoot += Time.deltaTime;
+        float t = timerToShoot / maxTimeToShoot;
+        if (t >= 1f)
+            t = 1f;
+        // Lerp the color based on the time on screen
+        if (objMaterial != null)
+        {
+            Color emissionColor = Color.Lerp(startColor, endColor, t);
+            objMaterial.SetColor("_EmissionColor", emissionColor);
+        }
+        // Adjust the score value based on the time on screen
+        scoreValue = Mathf.CeilToInt(baseScoreValue * (1-t));
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         //if (!other.CompareTag("Objects"))
